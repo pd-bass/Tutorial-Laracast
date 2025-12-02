81be1c915aa2a38dbdc79964862b810dc960f208
Sebastian Heuckmann <info@sebastianheuckmann.de>
2025-12-02 10:47:20 +0100

    Starting on php with Laracast

---
+++ demo/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello headline";
+        ?>
+    </h1>
+    <p>
+
+        <?php 
+            echo "Hello World in php"; 
+        ?>
+
+    </p>
+</body>
+</html>

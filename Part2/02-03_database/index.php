27cbdcc961d293af9b2193abc5bd2d14f9b8298c
Sebastian Heuckmann <info@sebastianheuckmann.de>
2026-02-01 13:43:04 +0100

    starting on sql

--- Part2/02-03_database/index.php
+++ Part2/02-03_database/index.php
@@ -2,4 +2,17 @@
 
 require "functions.php";
 
-require 'router.php';
+// require 'router.php';
+
+// connect to MySQL Database
+
+$dsn = "mariadb:host=localhost;port=3306;dbname=my_first_db;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);

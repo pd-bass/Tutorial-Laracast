f2b67f3d67c0f0b31858bd7891c90cea2b4cc134
Sebastian Heuckmann <info@sebastianheuckmann.de>
2026-03-03 15:22:36 +0100

    added a simple service container

--- Part3/03_project/public/index.php
+++ Part3/03_project/public/index.php
@@ -2,14 +2,15 @@
 
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH . 'Core/functions.php';
+require BASE_PATH.'Core/functions.php';
 
 spl_autoload_register(function ($class) {
-    // Core\Database required
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
-   require base_path("{$class}.php");
+    require base_path("{$class}.php");
 });
+
+require base_path('bootstrap.php');
 
 $router = new \Core\Router();
 

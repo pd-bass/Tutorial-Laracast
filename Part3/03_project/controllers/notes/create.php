c4a07b2d49c9123239cd065159b011b710d045e6
Sebastian Heuckmann <info@sebastianheuckmann.de>
2026-03-03 14:45:37 +0100

    one request, one controller

--- Part3/03_project/controllers/notes/create.php
+++ Part3/03_project/controllers/notes/create.php
@@ -1,28 +1,5 @@
 <?php
 
-use Core\Database;
-use Core\Validator;
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-
-$errors = [];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-
-    if (! Validator::string($_POST['body'], 1, 1000)) {
-        $errors['body'] = 'A body of no more than 1000 characters is required';
-    }
-
-    if (empty($errors)){
-        $db->query('INSERT INTO notes (title, body, user_id) VALUES(:title, :body, :user_id )', [
-            'title' => $_POST['title'],
-            'body' => $_POST['body'],
-            'user_id' => 1
-        ]);
-    }
-}
-
 view('notes/create.view.php', [
     'heading' => 'Create Note',
     'errors' => $errors,
---
+++ Part3/03_project/controllers/notes/store.php
@@ -0,0 +1,29 @@
+<?php
+
+use Core\Validator;
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$errors = [];
+
+if (!Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1000 characters is required';
+}
+if (!empty($errors)) {
+    return view('notes/create.view.php', [
+        'heading' => 'Create Note',
+        'errors' => $errors,
+    ]);
+}
+
+$db->query('INSERT INTO notes (title, body, user_id) VALUES(:title, :body, :user_id )', [
+    'title' => $_POST['title'],
+    'body' => $_POST['body'],
+    'user_id' => 1
+]);
+
+header('location: /notes');
+die();
+
